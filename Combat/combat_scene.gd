fcd97651d0486d4c4aeb401da5962fa653f54768
mabanana <aldwin.yu@hotmail.com>
2025-02-04 23:13:34 -0500

    Add main screen and auto battle

--- Combat/combat_scene.gd
+++ Combat/combat_scene.gd
@@ -10,35 +10,45 @@ var timer = Timer.new()
 var player_turn
 var combat_ended
 var outcome
+var auto_progress
 
-const scroll_speed = 0.08
+@onready var auto_toggle: CheckButton = %CheckButton
+@onready var combat_log_container: VBoxContainer = %VBoxContainer
+
+const scroll_speed = 0.1
 
 enum Outcome {
 	win,
-	lose,
-	tie,
-	run,
-	none,
+	loss,
+	unresolved,
 }
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	player_turn = true
 	combat_ended = false
-	outcome = Outcome.none
+	auto_progress = false
+	outcome = Outcome.unresolved
 	main = get_parent()
 	init_test()
 	start_combat_log_timer(scroll_speed)
-	
+	auto_toggle.toggled.connect(func(toggled_on):
+		auto_progress = toggled_on
+		auto_toggle.release_focus()
+	)
+	timer.timeout.connect(func():
+		if auto_progress:
+			progress_turn()
+	)
 
 func init_test():
 	add_character(CombatCharacter.new("Player", load("res://Combat/char_resources/player.tres")))
 	add_character(CombatCharacter.new("Bandit", load("res://Combat/char_resources/generic_human.tres")))
-	character_sprites[2].position = Vector2(200,100)
-	character_sprites[1].position = Vector2(-200,100)
+	character_sprites[2].position = Vector2(300, -100)
+	character_sprites[1].position = Vector2(-300, -100)
 	characters[2].enemy = true
 
-func _input(event):
+func _unhandled_input(event):
 	var result: Dictionary = main.input_handler.handle_input(event)
 	if result and result.context == InputHandler.Context.key_pressed or result.context == InputHandler.Context.mouse_pressed:
 		match result["action"]:
@@ -53,7 +63,6 @@ func handle_attack(dealer, target):
 		combat_log("%s is dead and cannot attack" % [dealer.name])
 		return
 	
-	combat_log("%s attacks %s" % [dealer.name, target.name])
 	var attack_roll = dealer.attack_roll()
 	var damage_roll = dealer.damage_roll()
 	
@@ -62,16 +71,15 @@ func handle_attack(dealer, target):
 		combat_log("%s dealt %s to %s" % [dealer.name, damage_roll, target.name])
 	else:
 		var defense_roll = target.defense_roll()
-		combat_log("Attack Roll: %s, Defense Roll: %s" % [attack_roll, defense_roll])
+		
 		if attack_roll > defense_roll:
 			target.take_damage(damage_roll)
-			combat_log("%s dealt %s to %s" % [dealer.name, damage_roll, target.name])
+			combat_log("%s attacks %s and dealt %s damage" % [dealer.name, target.name, damage_roll])
 		else:
 			combat_log("%s's attack missed" % [dealer.name])
 		if target.hp <= 0:
 			combat_log("%s has died" % [target.name])
 			target.alive = false
-	combat_log("%s: %s, %s: %s" % [dealer.name, dealer.hp, target.name, target.hp])
 	dealer_sprite.update()
 	target_sprite.update()
 
@@ -91,9 +99,11 @@ func _print_combat_log():
 		var label = Label.new()
 		label.text = text
 		label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
-		%VBoxContainer.add_child(label)
-		if %VBoxContainer.get_child_count() > 20:
-			%VBoxContainer.get_child(0).queue_free()
+		combat_log_container.add_child(label)
+		await get_tree().process_frame
+		combat_log_container.get_parent().ensure_control_visible(label)
+		if combat_log_container.get_child_count() > 30:
+			combat_log_container.get_child(0).queue_free()
 
 func add_character(character):
 	characters[id_counter] = character
@@ -121,11 +131,13 @@ func check_win():
 		outcome = Outcome.win
 		combat_ended = true
 	elif !player_alive and enemy_alive:
-		outcome = Outcome.lose
+		outcome = Outcome.loss
 		combat_ended = true
 
 func progress_turn():
 	if combat_ended:
+		main.main_menu.show()
+		main.conclude_combat(outcome)
 		queue_free()
 	
 	var player_char_list = []
@@ -136,7 +148,7 @@ func progress_turn():
 				enemy_char_list.append(char.id)
 			else:
 				player_char_list.append(char.id)
-
+	# TODO add targeting system, currently just attacking first on list
 	if player_turn:
 		for id in player_char_list:
 			var dealer: CombatCharacter = characters[id]
@@ -149,5 +161,13 @@ func progress_turn():
 			handle_attack(dealer, target)
 	check_win()
 	if combat_ended:
-		combat_log("Combat ended in %s.\nClick to continue..." % [Outcome.keys()[outcome]])
+		match Outcome:
+			Outcome.win:
+				combat_log("You Won!")
+			Outcome.loss:
+				combat_log("You Lost.")
+			Outcome.unresolved:
+				combat_log("This isn't over.")
+		combat_log("Click to continue...")
+		auto_toggle.button_pressed = false
 	player_turn = !player_turn
--- main.gd
+++ main.gd
@@ -3,14 +3,31 @@ class_name Main
 
 var input_handler: InputHandler = InputHandler.new()
 var current_scene: Node2D
+var main_menu: Control
+var wins = 0
+var losses = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	current_scene = load("res://Combat/scene.tscn").instantiate()
-	add_child(current_scene)
-	
-
+	main_menu = %Control
+	%Start_Combat.pressed.connect(start_combat)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func start_combat():
+	main_menu.hide()
+	current_scene = load("res://Combat/scene.tscn").instantiate()
+	add_child(current_scene)
+	if %CheckButton.button_pressed:
+		current_scene.auto_toggle.button_pressed = true
+
+func conclude_combat(outcome):
+	if outcome == CombatScene.Outcome.win:
+		wins += 1
+	elif outcome == CombatScene.Outcome.loss:
+		losses += 1
+	elif outcome == CombatScene.Outcome.unresolved:
+		print("unresolved combat")
+	%WinLoss.text = str(wins) + "/" + str(losses)
